51079d563e3537c8e255c4c27b9126b64b482e96
Chris McKenzie <kristopolous@yahoo.com>
2013-03-01 23:51:00 -0800

    added a script to create fortune

---
+++ tools/to-fortune.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+# Minimum score to include
+MINSCORE=200
+
+# First create the sqlite3 db with the python script
+
+./to-mysql.py
+
+# now create a fortune 'db' using all quotes above a certain threshhold
+
+sqlite3 ../sql/sqlite3.db "select quote from quotes where score > $MINSCORE" | sed -E 's/^$/%/' > ../fortune/bashorg
+
+(
+  cd ../fortune
+  strfile bashorg bashorg.dat
+)
